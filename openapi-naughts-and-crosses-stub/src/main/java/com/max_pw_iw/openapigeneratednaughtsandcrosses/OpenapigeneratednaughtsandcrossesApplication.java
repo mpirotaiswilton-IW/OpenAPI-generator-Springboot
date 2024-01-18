6c74748152f00581de796fceacd243445fd3b0f9
mpirotaiswilton-IW <max.pirotais-wilton@integration.works>
2024-01-18 13:19:58 +1300

    Migrating more of openapi springboot
    These files had names that were too long

---
+++ openapi-naughts-and-crosses-stub/src/main/java/com/max_pw_iw/openapigeneratednaughtsandcrosses/OpenapigeneratednaughtsandcrossesApplication.java
@@ -0,0 +1,13 @@
+package com.max_pw_iw.openapigeneratednaughtsandcrosses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapigeneratednaughtsandcrossesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapigeneratednaughtsandcrossesApplication.class, args);
+	}
+
+}

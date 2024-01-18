7ebdc04fae40291ac93bb1171bcfc0227e50e7fa
mpirotaiswilton-IW <max.pirotais-wilton@integration.works>
2024-01-18 13:20:52 +1300

    Create OpenapiApplicationTests.java

---
+++ openapi-naughts-and-crosses-stub/src/test/java/com/max_pw_iw/openapigeneratednaughtsandcrosses/OpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max_pw_iw.openapigeneratednaughtsandcrosses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapigeneratednaughtsandcrossesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
